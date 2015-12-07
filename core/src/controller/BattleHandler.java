f6031e0b339f7495eb95695b898969b9bfdcfa3f
Mikael.Mathis <mikaelmathis@yahoo.de>
2015-12-07 12:37:53 +0100

    Created BattleHandler
    blubb

---
+++ core/src/controller/BattleHandler.java
@@ -0,0 +1,15 @@
+package controller;
+
+import actor.Enemy;
+import actor.Player;
+
+/**
+ * Created by mikael.mathis on 04.12.2015.
+ */
+public final class BattleHandler {
+
+    public static double calculateDamage(Player player){
+        return 0;
+    }
+
+}
