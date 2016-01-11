277af0edaedb229d92093d51dc91f66061a591c3
Mikael.Mathis <mikaelmathis@yahoo.de>
2016-01-11 14:33:19 +0100

    Added Damage Text

--- core/src/controller/BattleRenderer.java
+++ core/src/controller/BattleRenderer.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -16,8 +15,14 @@ import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
+import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -26,6 +31,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.mygdx.game.MyGdxGame;
 
 import java.awt.event.InputEvent;
+import java.util.Random;
 
 public class BattleRenderer {
 
@@ -45,6 +51,8 @@ public class BattleRenderer {
 	private Texture enemyTexture;
 	private Texture goldTexture;
 	private Texture background;
+
+    private Label enemyHealthLabel;
 
 	private Rectangle enemyRectangle;
     private boolean enemyMovesUp;
@@ -100,6 +108,10 @@ public class BattleRenderer {
         initBattleScreen();
 
         enemyMovesUp = true;
+
+        enemyHealthLabel = new Label(String.valueOf(enemy.getLifePoints()), skin);
+        enemyHealthLabel.setPosition((Gdx.graphics.getWidth() / 2) - (enemyHealthLabel.getWidth() / 2), (Gdx.graphics.getHeight() / 10) * 8);
+        stage.addActor(enemyHealthLabel);
 
 	}
 
@@ -158,6 +170,7 @@ public class BattleRenderer {
 
 		updateEnemy(delta);
 
+        enemyHealthLabel.setText(String.valueOf(enemy.getLifePoints()));
         healthBar.setHealth(enemy.getLifePoints(), enemy.getMaxLifePoints());
 
 		cam.update();
@@ -240,8 +253,8 @@ public class BattleRenderer {
         enemyRectangle = new Rectangle();
         enemyRectangle.setSize(ENEMY_WIDTH, ENEMY_HEIGHT);
         enemyRectangle.setPosition(
-				Gdx.graphics.getWidth() / 2 - enemyRectangle.getWidth() / 2,
-				Gdx.graphics.getHeight() / 2 - enemyRectangle.getHeight() / 2);
+                Gdx.graphics.getWidth() / 2 - enemyRectangle.getWidth() / 2,
+                Gdx.graphics.getHeight() / 2 - enemyRectangle.getHeight() / 2);
 	}
 
 	public Texture chooseVisibleDMG() {
@@ -256,17 +269,44 @@ public class BattleRenderer {
 
 	}
 
-	public void damageEnemy() {
+	public float damageEnemy() {
 
-		EnemyHandler.damageEnemy(enemy);
+		return EnemyHandler.damageEnemy(enemy);
 
 	}
 
 	public void enemyClicked() {
 
-		damageEnemy();
+		float damage = damageEnemy();
+
+        Label damageLabel = new Label(String.valueOf(damage), skin);
+        damageLabel.setPosition(Gdx.graphics.getWidth() / 2, (Gdx.graphics.getWidth() / 2) - (damageLabel.getWidth() / 2));
+        stage.addActor(damageLabel);
+        createBouncingActor(damageLabel);
+        createFadingActor(damageLabel);
+
 		dmgVisible= true;
 	}
+
+    public void createBouncingActor(Actor actor){
+
+        Random rand = new Random();
+
+        float maxY = Gdx.graphics.getHeight() / 2;
+        float maxX = Gdx.graphics.getWidth() / 2;
+
+        MoveToAction action = Actions.action(MoveToAction.class);
+        action.setPosition(rand.nextFloat() * (maxX - 0) + 0, rand.nextFloat() * (maxY - 0) + 0);
+        action.setDuration(100f);
+        action.setInterpolation(Interpolation.bounceOut);
+        actor.addAction(action);
+    }
+
+    public void createFadingActor(Actor actor){
+
+        SequenceAction action = Actions.sequence(Actions.alpha(1), Actions.fadeOut(100f));
+        actor.addAction(action);
+    }
 
 	public class UserInputProcessor implements InputProcessor {
 		@Override
@@ -353,6 +393,11 @@ public class BattleRenderer {
         selectBoxStyleStyle.background = skin.newDrawable("background", Color.BLACK);
         selectBoxStyleStyle.scrollStyle = new ScrollPane.ScrollPaneStyle();
         skin.add("default", selectBoxStyleStyle);
+
+        Label.LabelStyle labelStyle = new Label.LabelStyle();
+        labelStyle.font = skin.getFont("default");
+//        labelStyle.background = skin.newDrawable("background", Color.WHITE);
+        skin.add("default", labelStyle);
 
     }
 }
